4a470e4a2b94459c7df7a4790b0588300554446e
Filipe Oliveira <contato@fmoliveira.com.br>
2016-11-06 13:42:22 -0200

    Fixed error mapping on objects store.

--- src/database/store_object.js
+++ src/database/store_object.js
@@ -3,12 +3,14 @@ export function storeObject(db, data) {
     const promises = data.map(i => store(db, i))
     return Promise.all(promises)
   } else {
-    return store(data)
+    return store(db, data)
   }
 }
 
 function store(db, i) {
   const col = i.code >= 200 && i.code < 300 ? 'objects' : 'errors'
-  i.body = JSON.parse(i.body)
+  if (i.body && typeof i.body === 'string') {
+    i.body = JSON.parse(i.body)
+  }
   return db.collection(col).insert(i)
 }
