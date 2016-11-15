18c519272761e980080898e334915cb64ff34795
Filipe Oliveira <contato@fmoliveira.com.br>
2016-11-15 11:07:33 -0200

    Storing page result into the pages collection.

--- src/database/store_object.js
+++ src/database/store_object.js
@@ -11,9 +11,44 @@ export function storeObject(db, data) {
 }
 
 function store(db, i) {
-  const col = i.code >= 200 && i.code < 300 ? STORE_OBJECT_QUEUE : STORE_ERROR_QUEUE
   if (i.body && typeof i.body === 'string') {
     i.body = JSON.parse(i.body)
   }
-  return db.collection(col).insert(i)
+
+  if (i.code >= 200 && i.code < 300) {
+    if (i.body && i.body.category) {
+      return updatePage(db, i.body)
+    } else {
+      return db.collection(STORE_OBJECT_QUEUE).insert(i)
+    }
+  } else {
+    return db.collection(STORE_ERROR_QUEUE).insert(i)
+  }
+}
+
+function updatePage(db, i) {
+  return db.collection('pages').update({ fb_account_id: i.id }, {
+    $set: {
+      about: i.about,
+      category: i.category,
+      category_list: i.category_list,
+      cover: i.cover,
+      emails: i.emails,
+      engagement: i.engagement,
+      fan_count: i.fan_count,
+      is_published: i.is_published,
+      is_verified: i.is_verified,
+      is_webhooks_subscribed: i.is_webhooks_subscribed,
+      link: i.link,
+      location: i.location,
+      name: i.name,
+      overall_star_rating: i.overall_star_rating,
+      phone: i.phone,
+      rating_count: i.rating_count,
+      talking_about_count: i.talking_about_count,
+      verification_status: i.verification_status,
+      voip_info: i.voip_info,
+      were_here_count: i.were_here_count
+    }
+  })
 }
