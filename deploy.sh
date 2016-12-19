283055251f9a0cb822e5c7f4c150f1248973b36a
Filipe Oliveira <contato@fmoliveira.com.br>
2016-12-19 01:18:43 +0000

    Authenticating RabbitMQ connections.

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ docker run \
   --link mob-db-mongo:db \
   --link mob-mq-rabbit:mq \
   -e MOB_MONGO_FACEBOOK_DATABASE='mongodb://db:27017/mobyourlife_facebook' \
-  -e MOB_RABBITMQ_URL='amqp://mq' \
+  -e MOB_RABBITMQ_URL="amqp://$MOB_RABBITMQ_USERNAME:$MOB_RABBITMQ_PASSWORD@mq" \
   -e FACEBOOK_APP_ID=$FACEBOOK_APP_ID \
   -e FACEBOOK_APP_SECRET=$FACEBOOK_APP_SECRET \
   -d \
